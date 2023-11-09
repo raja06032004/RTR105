28f2219ea8cf3117643d699b4fbc24a248c05b27
raja06032004 <144798276+raja06032004@users.noreply.github.com>
2023-11-09 15:58:39 +0200

    Update homework_02.c

--- Homework_2910/homework_02.c
+++ Homework_2910/homework_02.c
@@ -1,24 +1,20 @@
-#include<stdio.h>     
-#include<stdint.h>     
-#include<math.h>        
+include<stdio.h>
+#include<stdint.h>  
+#include<math.h>
 int main()       
 {
-printf("Enter a natural number: ");
-  int a;      
-  scanf("%u", &a);
-
-  printf("Number: 0x%X\n", a);
-  
-  printf("%d", ((a & 0x80) >> 7));
-  printf("%d", ((a & 0x40) >> 6));
-  printf("%d", ((a & 0x20) >> 5));
-  printf("%d", ((a & 0x10) >> 4));
-  printf("%d", ((a & 0x08) >> 3));
-  printf("%d", ((a & 0x04) >> 2));
-  printf("%d", ((a & 0x02) >> 1));
-  printf("%d", ((a & 0x01) >> 0));
-
-  printf("\n");
-  
+char a;
+printf("Enter a natural number: ");      
+scanf("%hhd", &a);
+printf("Number: 0x%X\n", a);
+printf("%hhd", ((a & 0x80) >> 7));
+printf("%hhd", ((a & 0x40) >> 6));
+printf("%hhd", ((a & 0x20) >> 5));
+printf("%hhd", ((a & 0x10) >> 4));
+printf("%hhd", ((a & 0x08) >> 3));
+printf("%hhd", ((a & 0x04) >> 2));
+printf("%hhd", ((a & 0x02) >> 1));
+printf("%hhd", ((a & 0x01) >> 0));
+printf("\n");
   return 0;      
-}      
+}    
