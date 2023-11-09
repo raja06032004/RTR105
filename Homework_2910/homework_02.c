90f89775fdcc382e1ae4fd62d8da0607728229dc
raja06032004 <144798276+raja06032004@users.noreply.github.com>
2023-11-09 19:17:43 +0200

    Update homework_02.c

--- Homework_2910/homework_02.c
+++ Homework_2910/homework_02.c
@@ -11,7 +11,7 @@ printf("%hhd", ((a>>7) & 0x01) );
 printf("%hhd", ((a>>6) & 0x01) );
 printf("%hhd", ((a>>5) & 0x01) );
 printf("%hhd", ((a>>4) & 0x01) );
-printf("%hhd", ((a>>3  & 0x01) );
+printf("%hhd", ((a>>3) & 0x01) );
 printf("%hhd", ((a>>2) & 0x01) );
 printf("%hhd", ((a>>1) & 0x01) );
 printf("%hhd", ((a>>0) & 0x01) );
