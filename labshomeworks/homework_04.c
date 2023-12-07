78075736126278b2cfc795e2722d4c0cee2e04aa
raja06032004 <my_ganesanrajaganapathy8228@gmail.com>
2023-12-07 18:09:33 +0200

    Class_19_

--- labshomeworks/homework_04.c
+++ labshomeworks/homework_04.c
@@ -10,77 +10,49 @@ void calculateFactorial(char dataType, double number)
 
 
     // Checking user's selected data type and calculating factorial accordingl
-
     switch (dataType) {
+    case 'c':
+    while (i <= number) {
+         charResult *= i;
+    if (charResult < 0) {
+        printf("Factorial value exceeds range for char data type.\n");
 
-        case 'c':
-
-            while (i <= number) {
-
-                charResult *= i;
-
-                if (charResult < 0) {
-
-                    printf("Factorial value exceeds range for char data type.\n");
-
-                    return;
-
+         return;
                 }
-
                 i++;
-
             }
 
             printf("Factorial of %.0lf using char data type: %d\n", number, charResult);
-
             break;
 
         case 'i':
-
-            while (i <= number) {
-
+        while (i <= number) {
                 intResult *= i;
+        if (intResult < 0) {
+            printf("Factorial value exceeds range for int data type.\n");
 
-                if (intResult < 0) {
-
-                    printf("Factorial value exceeds range for int data type.\n");
-
-                    return;
-
-                }
-
+            return;
+               }
                 i++;
 
             }
 
             printf("Factorial of %.0lf using int data type: %d\n", number, intResult);
-
-            break;
+       break;
 
         case 'l':
+        while (i <= number) {
+               longLongResult *= i;
 
-            while (i <= number) {
-
-                longLongResult *= i;
-
-                if (longLongResult < 0) {
-
-                    printf("Factorial value exceeds range for long long data type.\n");
-
-                    return;
-
+         if (longLongResult < 0) {
+             printf("Factorial value exceeds range for long long data type.\n");
+      return;
                 }
-
                 i++;
-
             }
-
             printf("Factorial of %.0lf using long long data type: %lld\n", number, longLongResult);
-
-            break;
-
+      break;
         default:
-
             printf("Invalid data type selected.\n");
 
     }
@@ -90,28 +62,13 @@ void calculateFactorial(char dataType, double number)
 
 
 int main() {
-
     double userInput;
-
     char dataType;
-
-
-
     printf("Enter a decimal number: ");
-
     scanf("%lf", &userInput);
-
-
-
     printf("Select data type - char (c), int (i), or long long (l): ");
-
     scanf(" %c", &dataType);
-
-
-
     calculateFactorial(dataType, userInput);
-
-
 
     return 0;
 
--- labshomeworks/homework_05.c
+++ labshomeworks/homework_05.c
@@ -1,83 +1,52 @@
 #include <stdio.h>
-
-
-
 void calculateFactorial(char dataType, double number) {
 
-    // Initialize result variables based on the selected data type
-
     char charResult = 1;
-
     int intResult = 1;
-
     long long longLongResult = 1;
 
-
-
     switch (dataType) {
-
-        case 'c':
-
-            for (int i = 1; i <= number; ++i) {
-
+    case 'c':
+     for (int i = 1; i <= number; ++i) {
                 charResult *= i;
-
                 if (charResult == 0) {
-
                     printf("Factorial value exceeds range for char data type.\n");
-
                     return;
-
                 }
-
             }
 
             printf("Factorial of %.0lf using char data type: %d\n", number, charResult);
+       break;
 
-            break;
-
-        case 'i':
-
-            for (int i = 1; i <= number; ++i) {
-
-                intResult *= i;
-
-                if (intResult == 0) {
-
-                    printf("Factorial value exceeds range for int data type.\n");
-
-                    return;
-
+       case 'i':
+       for (int i = 1; i <= number; ++i) {
+             intResult *= i;
+       if (intResult == 0) {
+          printf("Factorial value exceeds range for int data type.\n");
+       return;
                 }
+       }
 
-            }
-
-            printf("Factorial of %.0lf using int data type: %d\n", number, intResult);
-
-            break;
+           printf("Factorial of %.0lf using int data type: %d\n", number, intResult);
+        break;
 
         case 'l':
+        for (int i = 1; i <= number; ++i) {
+             longLongResult *= i;
 
-            for (int i = 1; i <= number; ++i) {
+        if (longLongResult == 0)
+       {
+           printf("Factorial value exceeds range for long long data type.\n");
 
-                longLongResult *= i;
-
-                if (longLongResult == 0) {
-
-                    printf("Factorial value exceeds range for long long data type.\n");
-
-                    return;
-
-                }
-
+         return;
+         }
             }
 
             printf("Factorial of %.0lf using long long data type: %lld\n", number, longLongResult);
 
-            break;
+          break;
 
         default:
-
             printf("Invalid data type selected.\n");
 
     }
@@ -85,30 +54,16 @@ void calculateFactorial(char dataType, double number) {
 }
 
 
-
 int main() {
-
     double userInput;
-
     char dataType;
-
-
-
     printf("Enter a decimal number: ");
-
     scanf("%lf", &userInput);
 
-
-
     printf("Select data type - char (c), int (i), or long long (l): ");
-
     scanf(" %c", &dataType);
 
-
-
     calculateFactorial(dataType, userInput);
-
-
 
     return 0;
 
