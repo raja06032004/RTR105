533dcf38b008f03cead5e2630e1b6b34a708c9ed
raja06032004 <144798276+raja06032004@users.noreply.github.com>
2024-01-19 15:58:43 +0200

    Update With__Arguments_and_Without_Return_value.c

--- labshomeworks/With__Arguments_and_Without_Return_value.c
+++ labshomeworks/With__Arguments_and_Without_Return_value.c
@@ -1,39 +1,18 @@
 #include <stdio.h>
 
-
-
 // Function with arguments and without return
-
 void printSum(int a, int b) {
-
     printf("Sum of %d and %d is: %d\n", a, b, a + b);
-
 }
 
-
-
 int main() {
-
     int x, y;
-
-
-
     printf("Enter the x integer: ");
-
     scanf("%d", &x);
-
-
-
     printf("Enter the y integer: ");
-
     scanf("%d", &y);
 
-
-
     // Call the function with arguments and without return
-
     printSum(x, y);
-
     return 0;
-
 }
