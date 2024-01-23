9fd3657d1d2fc056f6a1cad3fa7ce9be4cb6445b
raja06032004 <144798276+raja06032004@users.noreply.github.com>
2024-01-23 21:47:56 +0200

    Create LAB_02.c

---
+++ labshomeworks/LAB_02.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char num;
+    // Prompt the user to enter a natural number
+    printf("Enter a natural number: ");
+    scanf("%hhd", &num);
+
+    if (num < 0) {
+        printf("Please enter a non-negative number.\n");
+        return 1;
+    }
+    printf("Binary representation of %d: ", num);
+    // Size of char in bits (usually 8)
+   int bits = sizeof(char) * 8;
+
+    // Using bitwise shift and mask to print the binary representation
+    for (int i = bits - 1; i >= 0; i--) {
+        char bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
+    return 0;
+}
+
+
