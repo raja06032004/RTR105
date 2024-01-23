470b5106ad4ec2ee757669baff6903f774876194
raja06032004 <144798276+raja06032004@users.noreply.github.com>
2024-01-24 00:26:33 +0200

    Create main.c

---
+++ labshomeworks/main.c
@@ -0,0 +1,37 @@
+\#include <stdio.h>
+#include "functions.h"
+
+/*
+int generateRandomNumber();
+int calculateProduct(int a, int b) ;
+void printSum(int a, int b);
+void greetUser();
+*/
+
+int main() {
+    int randomNumber, x, y, product;
+
+    // Call the function without arguments and with return
+    randomNumber = generateRandomNumber();
+    printf("Random number generated: %d\n", randomNumber);
+
+    // Call the function without arguments and without return
+    greetUser();
+
+    // Call the function with arguments and with return
+    printf("Enter the x integer: ");
+    scanf("%d", &x);
+    printf("Enter the y integer: ");
+    scanf("%d", &y);
+    product = calculateProduct(x, y);
+    printf("Product of %d and %d is: %d\n", x, y, product);
+
+    // Call the function with arguments and without return
+    printf("Enter the x integer: ");
+    scanf("%d", &x);
+    printf("Enter the y integer: ");
+    scanf("%d", &y);
+    printSum(x, y);
+
+    return 0;
+}
