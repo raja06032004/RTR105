f1ac1a4c97a798594fb5f928cd5ffc4db769058f
raja06032004 <144798276+raja06032004@users.noreply.github.com>
2024-01-24 01:01:47 +0200

    Update and rename function.h to functions.h

--- labshomeworks/function.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H_
-#define FUNCTIONS_H_
-
-int generateRandomNumber();
-int calculateProduct(int a, int b) ;
-void printSum(int a, int b);
-void greetUser();
-
-#endif //FUNCTIONS_H_
---
+++ labshomeworks/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int generateRandomNumber();
+int calculateProduct(int a, int b);
+void printSum(int a, int b);
+void greetUser();
+
+#endif
