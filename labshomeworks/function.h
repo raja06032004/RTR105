6f20c0c727f9ed4c31fba6cd81b74eb160861546
raja06032004 <144798276+raja06032004@users.noreply.github.com>
2024-01-24 00:24:25 +0200

    Create function.h

---
+++ labshomeworks/function.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+int generateRandomNumber();
+int calculateProduct(int a, int b) ;
+void printSum(int a, int b);
+void greetUser();
+
+#endif //FUNCTIONS_H_
